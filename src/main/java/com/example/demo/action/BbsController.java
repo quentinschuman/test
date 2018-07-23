33c138600cddaa008a232d4b2a648ddb1c8e59fc
qianshu <qianshu@hikvision.com.cn>
2018-07-23 17:28:18 +0800

    [debug]beetl properties

--- src/main/java/com/example/demo/action/BbsController.java
+++ src/main/java/com/example/demo/action/BbsController.java
@@ -2,12 +2,16 @@ package com.example.demo.action;
 
 import com.example.demo.common.WebUtils;
 import com.example.demo.es.service.EsService;
+import com.example.demo.es.vo.IndexObject;
 import com.example.demo.service.BbsService;
 import com.example.demo.service.BbsUserService;
+import org.apache.commons.lang3.StringUtils;
 import org.beetl.sql.core.SQLManager;
+import org.beetl.sql.core.engine.PageQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -45,5 +49,27 @@ public class BbsController {
         return new ModelAndView("forward:/bbs/topic/module/1-1.html");
     }
 
+    @RequestMapping("/bbs/index")
+    public ModelAndView index(HttpServletRequest request){
+        return new ModelAndView("forward:/bbs/index/1.html");
+    }
 
+    @RequestMapping("/bbs/index/{p}.html")
+    public ModelAndView index(@PathVariable int p,String keyword){
+        ModelAndView view = new ModelAndView();
+        if (StringUtils.isBlank(keyword)){
+            view.setViewName("/index.html");
+            PageQuery query = new PageQuery(p,null);
+            query = bbsService.getTopics(query);
+            view.addObject("topicPage",query);
+            view.addObject("pagename","首页综合");
+        }else {
+            PageQuery<IndexObject> searcherKeywordPage = this.esService.getQueryPage(keyword,p);
+            view.setViewName("/lucene/lucene.html");
+            view.addObject("searcherPage", searcherKeywordPage);
+            view.addObject("pagename", keyword);
+            view.addObject("resultnum", searcherKeywordPage.getTotalRow());
+        }
+        return view;
+    }
 }
